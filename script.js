681c6b8aefa2a7826c06490b60107cb5bfd6c9ad
cmpowers1981 <c.m.powers@outlook.com>
2019-07-16 14:04:33 -0400

    Commit 3

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
-function renderShoppingList() {
-    console.log('`renderShoppingList` ran');
-}
+const STORE = [
+    {name: "Apples", checked: false},
+    {name: "Coconut", checked: false},
+    {name: "Coconut", checked: false},
+    {name: "Hamsters", checked: false},
+    {name: "Magazines", checked: false},
+    {name: "Broccoli", checked: false},
+]
 
-function handleNewItemSubmit() {
-    console.log('`handleNewItemSubmit` ran');
-}
+function generateItemElement(item, itemIndex, template) {
+    return `
+        <li class="js-item-index-element" data-item-index="${itemIndex}">
+            <span class="shopping-item js-shopping-item ${item.checked ? "shopping-item_checked" : ''}">${item.name}</span>
+            <div class="shopping-item-controls">
+                <button class="shopping-item-toggle js-item-toggle">
+                    <span class="butt-label>Submit</span>
+                </button>
+            </div>
+        </li>`;
+    }
 
-function handleItemCheckClicked() {
-    console.log('`handleItemCheckClicked` ran');
-}
+    function generateItemsString(shoppingList) {
+        console.log("Generate Shopping List Element");
 
-function handleDeleteItemClicked() {
-    console.log('`handleDeleteItemClicked` ran')
-}
+        const items = shoppingList.map((item, index) => generateItemElement(item, index));
 
-function handleShoppingList() {
+        return items.join("");
+    }
 
-}
+    function renderShoppingList() {
+        console.log('`renderShoppingList` ran');
+        const shoppingListItemsString = generateShoppingItemsString(STORE);
+
+        $('.js-shopping-list').html(shoppingListItemsString);
+    }
+
+    function addItemToShoppingList(itemName) {
+        console.log(`Adding "${itemName}" to shopping list`);
+        STORE.push( {name: itemName, checked: false} );
+    }
+
+    function handleNewItemSubmit() {
+        $('#js-shopping-list-form').submit(function(event) {
+            event.preventDefault();
+            console.log('`handleNewItemSubmit` ran');
+            const newItemName = $('.js-shopping-list-entry').val();
+            $('.js-shopping-list-entry')
+        }
+    }
